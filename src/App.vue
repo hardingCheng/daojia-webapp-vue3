a21306e640c0691dba05470a0241c88ee7823499
hardingcheng <hardingcheng@163.com>
2021-12-21 12:29:43 +0800

    feat(首页): 首页底部Tab布局和样式

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <template>
-  <div id="nav">1231</div>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6f3;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe7e5;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe61e;</div>
+      <div class="docker__title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 0.18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: 0.18rem;
+    }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+  &__title {
+    font-size: 20px;
+    // 处理小于浏览器自己的默认字体的
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
+}
+</style>
